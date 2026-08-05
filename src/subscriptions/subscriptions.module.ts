758a44067e9c70ed06f84ec9db769c9c8bc8528e
Ajeet Kumar Sah <ajeetkumarsahin@gmail.com>
2026-08-05 15:56:41 +0530

    Subscription fixes

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -3,7 +3,10 @@ import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsRepository } from './subscriptions.repository';
 
+import { DatabaseModule } from '../database/database.module';
+
 @Module({
+  imports: [DatabaseModule],
   controllers: [SubscriptionsController],
   providers: [SubscriptionsService, SubscriptionsRepository],
   exports: [SubscriptionsService],
